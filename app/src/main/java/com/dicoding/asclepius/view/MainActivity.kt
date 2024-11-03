c04c2015ea5d2b2c4975be0f010b229c4f85a3bd
AlvinHartono <128015193+AlvinHartono@users.noreply.github.com>
2024-11-03 20:09:19 +0700

    feat: added image intent

--- app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.dicoding.asclepius.view
 
-import android.Manifest
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.dicoding.asclepius.R
+import androidx.activity.result.contract.ActivityResultContracts
 import com.dicoding.asclepius.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,17 +13,43 @@ class MainActivity : AppCompatActivity() {
 
     private var currentImageUri: Uri? = null
 
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+
+            currentImageUri = selectedImg
+            showToast("Gambar berhasil dipilih")
+            showImage()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.galleryButton.setOnClickListener {
+            startGallery()
+        }
     }
 
     private fun startGallery() {
         // TODO: Mendapatkan gambar dari Gallery.
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+
+        launcherIntentGallery.launch(chooser)
     }
 
     private fun showImage() {
         // TODO: Menampilkan gambar sesuai Gallery yang dipilih.
+        currentImageUri?.let {
+            binding.previewImageView.setImageURI(it)
+        }
     }
 
     private fun analyzeImage() {
